f5b1ca064a5ba5fd6a5fd15c24085961a0e1dff1
rusty-jar <136445039+rusty-jar@users.noreply.github.com>
2023-06-24 15:05:49 +0530

    Fix comment lines

--- src/Sem-1/Assignment-5/main.c
+++ src/Sem-1/Assignment-5/main.c
@@ -2,7 +2,7 @@
 Copyright (c) 2023 Shahibur Rahaman
 Licensed under GNU GPLv3
 
-Q|> Write a program to show Fibonacci sequence up to the given nth term.
+Q|> Write a program to show Fibonacci sequence up to nth term.
 
 Constraints:
   - Input value is an element belonging to the set of whole numbers.
@@ -11,26 +11,25 @@ Constraints:
 #include <stdio.h>
 
 void getFibSequence(int value) {
-  int i, prev_num = 0, curr_num = 1, next_num;
-
   printf("Fibonacci sequence = [ ");
-  // Print blank space to indicate no valid sequence
+  // Print blank space to indicate empty sequence.
   if (value == 0) {
     printf(" ");
   }
-  // Print the 1st element of the fibonacci sequence
+  // Print the 1st element of the fibonacci sequence.
   else if (value == 1) {
     printf("0 ");
   }
-  // Print the 1st and 2nd element of the fibonacci sequence
+  // Print the 1st and 2nd element of the fibonacci sequence.
   else if (value == 2) {
     printf("0 1 ");
   }
-  // Print the whole fibonacci sequence for nth terms
+  // Print the whole fibonacci sequence for 'n' terms.
   else {
+    int prev_num = 0, curr_num = 1, next_num;
     printf("%d %d ", prev_num, curr_num);
 
-    for (i = 3; i <= value; i++) {
+    for (int i = 3; i <= value; i++) {
       next_num = prev_num + curr_num;
       printf("%d ", next_num);
       if (i == value) {
